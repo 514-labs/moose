21bd6dfc6c2a07a5889074302a747d4292f46c1c
Nicolas Joseph <nicolas@nicolasjoseph.com>
2023-08-08 21:58:04 -0700

    fix: capitalization for output in github actions

--- apps/igloo-kit-cli/scripts/release-cli.sh
+++ apps/igloo-kit-cli/scripts/release-cli.sh
@@ -8,7 +8,7 @@ if [[ "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+-BUILD\.[0-9]+$ ]]; then
    npm version $version --no-git-tag-version
 
    # change all the dependencies in the package.json optionalDependencies to use 
-   #the BUILD version
+   # the BUILD version
    jq -r '.optionalDependencies | keys[]' package.json | while read dep; do
     #   pnpm up $dep $version
       jq --arg DEP "$dep" --arg VERSION "$version" '.["optionalDependencies"][$DEP] = $VERSION' package.json > package.json.tmp && mv package.json.tmp package.json

5e1effdf96903bcbd9fa572b9d73f62081fe9a3a
Nicolas Joseph <nicolas@nicolasjoseph.com>
2023-08-13 19:04:49 -0500

    fix: script wrapper dependency

--- apps/igloo-kit-cli/src/index.ts
+++ apps/igloo-kit-cli/src/index.ts
@@ -21,7 +21,7 @@ function getExePath() {
 
   try {
     // Since the binary will be located inside `node_modules`, we can simply call `require.resolve`
-    return require.resolve(`igloo-${os}-${arch}/bin/app${extension}`);
+    return require.resolve(`@514labs/igloo-cli-${os}-${arch}/bin/app${extension}`);
   } catch (e) {
     throw new Error(
       `Couldn't find application binary inside node_modules for ${os}-${arch}`

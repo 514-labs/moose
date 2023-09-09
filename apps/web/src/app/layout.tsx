7adde37f31ef1bff9b918942f1ff681c59d412fd
Tim <tim@cloudpantry.com>
2023-09-08 20:35:22 -0700

    added script for heap

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import "ui/styles.css";
 import localFont from 'next/font/local'
 import Head from 'next/head';
+import Script from 'next/script';
 
 import { Analytics } from '@vercel/analytics/react';
 
@@ -28,12 +29,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en"  className={"bg-black h-full " + `${monoFont.variable} ${sansFont.variable}`}>
-      <Head>
-      <script type="text/javascript">
-        window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-        heap.load("4099753721");
-      </script>
-      </Head>
+      <Script type="text/javascript">
+       {` window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+        heap.load("4099753721");`}
+      </Script>
       <body className="h-full font-sans font-regular">
         {children}
         <Analytics />

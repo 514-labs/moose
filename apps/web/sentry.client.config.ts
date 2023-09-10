bf4043823d6142a8de3f9cc242463211ebe8cfbe
Nicolas Joseph <nicolas@nicolasjoseph.com>
2023-09-09 21:10:57 -0700

    chore: trigger vercel build

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -1,6 +1,7 @@
 // This file configures the initialization of Sentry on the client.
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// Automatically generated
 
 import * as Sentry from "@sentry/nextjs";
 

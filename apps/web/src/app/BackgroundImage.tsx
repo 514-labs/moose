3df7b5091a249e1c78a12a185294da0fd074c3c1
Tim <tim@polarizedcapital.com>
2023-09-29 09:37:38 -0700

    added loading animation and size param

--- apps/web/src/app/BackgroundImage.tsx
+++ apps/web/src/app/BackgroundImage.tsx
@@ -7,22 +7,22 @@ import Image from "next/image";
 export const BackgroundImage = () => {
   const imageRef = React.useRef(null);
 
-  // useLayoutEffect(() => {
-  //   let ctx = gsap.context(() => {
-  //     gsap.set(imageRef.current, { visibility: "visible" });
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.set(imageRef.current, { visibility: "visible" });
       
-  //     gsap.from(imageRef.current,{
-  //       opacity: 0,
-  //       duration: 2,
-  //     });
-  //   });
-  //   return () => {
-  //     ctx.revert();
-  //   };
-  // }, []);
+      gsap.from(imageRef.current,{
+        opacity: 0,
+        duration: 2,
+      });
+    });
+    return () => {
+      ctx.revert();
+    };
+  }, []);
 
   return (
     // <div className="bg-[url('/bg_igloo_image_person_02_4x.webp')] brightness-50 flex h-full" ref={imageRef} />
-    <Image  src={'/bg_igloo_image_person_02_4x.webp'} className="object-cover" fill alt="developer in action"/>
+    <Image  src={'/bg_igloo_image_person_02_4x.webp'} className="invisible object-cover" fill alt="developer in action" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" ref={imageRef}/>
   );
 };

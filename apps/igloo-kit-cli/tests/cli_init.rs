1b49496d9b51192a56d46f2544c67715efe7a905
Nicolas Joseph <nicolas@nicolasjoseph.com>
2023-11-12 23:46:21 -0500

    chore: proper init test

--- apps/igloo-kit-cli/tests/cli_init.rs
+++ apps/igloo-kit-cli/tests/cli_init.rs
@@ -1,4 +1,5 @@
 use assert_cmd::prelude::*; // Add methods on commands
+use assert_fs::prelude::*;
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
@@ -16,16 +17,18 @@ fn cannot_run_igloo_init_without_args() -> Result<(), Box<dyn std::error::Error>
 
 #[test]
 fn can_run_igloo_init() -> Result<(), Box<dyn std::error::Error>> {
+    let temp = assert_fs::TempDir::new().unwrap();
+    let dir = temp.path().to_str().unwrap();
     let mut cmd = Command::cargo_bin("igloo-cli")?;
+
+    cmd.arg("init").arg("test-app").arg("ts").arg(dir);
+
+    cmd.assert().success();
 
     // TODO add more specific tests when the layout of the
     // app is more stable
-    cmd.arg("init")
-        .arg("test-app")
-        .arg("ts")
-        .arg("./tests/test-app");
-
-    cmd.assert().success();
+    temp.child(".igloo").assert(predicate::path::exists());
+    temp.child("app").assert(predicate::path::exists());
 
     Ok(())
 }

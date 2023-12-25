43e1f85d9f03df88d0f078e8591cc77812a6d7d4
Tim Delisle <9058816+tg339@users.noreply.github.com>
2023-12-25 14:24:57 -0800

    Tim/514-216-build-the-development-console (#108)
    * updated hyper 1.0 and added initial console
    
    * little bit of clean up
    
    * simplified routing logic
    
    * added error managemnet to moose console
    
    * updated sentry configuration
    
    ---------
    
    Co-authored-by: Tim Delisle <tim@fiveonefour.com>

---
+++ apps/moose-console/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://420ad9a138f36701dadb42fd8640357f@o4505851966128128.ingest.sentry.io/4506458108919808",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});

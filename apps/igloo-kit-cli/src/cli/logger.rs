5c0cb37c961138bbe185786f2b0cfc852b921d93
Tim Delisle <tim@fiveonefour.com>
2024-01-02 12:22:09 -0800

    clippy fix

--- apps/igloo-kit-cli/src/cli/logger.rs
+++ apps/igloo-kit-cli/src/cli/logger.rs
@@ -44,19 +44,19 @@ const LOG_FILE: &str = "cli.log";
 
 #[derive(Deserialize, Debug, Clone)]
 pub enum LoggerLevel {
-    DEBUG,
-    INFO,
-    WARN,
-    ERROR,
+    Debug,
+    Info,
+    Warn,
+    Error,
 }
 
 impl LoggerLevel {
     pub fn to_log_level(&self) -> log::LevelFilter {
         match self {
-            LoggerLevel::DEBUG => log::LevelFilter::Debug,
-            LoggerLevel::INFO => log::LevelFilter::Info,
-            LoggerLevel::WARN => log::LevelFilter::Warn,
-            LoggerLevel::ERROR => log::LevelFilter::Error,
+            LoggerLevel::Debug => log::LevelFilter::Debug,
+            LoggerLevel::Info => log::LevelFilter::Info,
+            LoggerLevel::Warn => log::LevelFilter::Warn,
+            LoggerLevel::Error => log::LevelFilter::Error,
         }
     }
 }
@@ -76,7 +76,7 @@ fn default_log_file() -> String {
 }
 
 fn default_log_level() -> LoggerLevel {
-    LoggerLevel::INFO
+    LoggerLevel::Info
 }
 
 impl Default for LoggerSettings {

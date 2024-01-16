c3735ef607b94c25fcc109ec19db9be54adf49aa
Nicolas Joseph <nicolas@nicolasjoseph.com>
2024-01-15 17:53:21 -0800

    wip docker for moose console

--- apps/moose-console/next.config.js
+++ apps/moose-console/next.config.js
@@ -1,5 +1,11 @@
+const path = require("path");
+
 module.exports = {
   output: "standalone",
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
   reactStrictMode: true,
 };
 

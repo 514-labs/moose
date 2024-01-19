17b5794705307b65543e9785df01c61d891e5043
Tim Delisle <9058816+tg339@users.noreply.github.com>
2024-01-18 20:06:40 -0800

    Initial theming and mocks (#136)
    Co-authored-by: Dan De Vries <DanDeVries11@users.noreply.github.com>
    Co-authored-by: George Leung <phiSgr@users.noreply.github.com>
    Co-authored-by: Alex <asl677@users.noreply.github.com>
    Co-authored-by: Chris Crane <03cranec@users.noreply.github.com>

---
+++ apps/moose-console/src/components/left-nav.tsx
@@ -0,0 +1,92 @@
+// Create a left nav from the navigation menu primitives in /ui/menu-bar.tsx
+
+interface NavLinks {
+    name: string;
+    href: string;
+}
+
+interface Section {
+    name: string;
+    href: string;
+    links: NavLinks[];
+}
+
+interface LeftNavProps {
+    sections: Section[];
+}
+
+
+// There are two sections, a "Primitives" section and a "Infrastructure" Section. 
+// 
+// The "Primitives" section has the following links:
+// models, flows and insights
+
+// The "Infrastructure" section has the following links:
+// Ingestion points
+// Queues
+// Tables
+// Views
+
+let sections: Section[] = [
+    {
+        name: "Primitives",
+        href: "/primitives",
+        links: [
+            {
+                name: "Models",
+                href: "/primitives/models",
+            },
+            {
+                name: "Flows",
+                href: "/primitives/flows",
+            },
+            {
+                name: "Insights",
+                href: "/primitives/insights",
+            },
+        ]
+    },
+    {
+        name: "Infrastructure",
+        href: "/infrastructure",
+        links: [
+            {
+                name: "Ingestion Points",
+                href: "/infrastructure/ingestion-points",
+            },
+            {
+                name: "Queues",
+                href: "/infrastructure/queues",
+            },
+            {
+                name: "Tables",
+                href: "/infrastructure/tables",
+            },
+            {
+                name: "Views",
+                href: "/infrastructure/views",
+            },
+        ]
+    }
+]
+
+
+export const LeftNav = () => {
+    return (
+        <div className="flex flex-col w-64 px-4 py-8">
+            {sections.map((section) => (
+                <div key={section.name} className="mb-10">
+                    <div className="text-sm font-black uppercase">{section.name}</div>
+                    <ul className="mt-3">
+                        {section.links.map((link) => (
+                            <li key={link.name} className="py-2">
+                                <a href={link.href} className=" hover:font-bold font-light block">{link.name}</a>
+                            </li>
+                        ))} 
+                    </ul>
+                </div>
+            )
+            )}
+        </div>
+    )
+}

e133642ac28895636b3ee21293a3813e75a00a44
cjus <carlos.justiniano@gmail.com>
2024-02-25 19:45:36 -0700

    check for required repository argument

--- apps/framework-cli/deploy/pseudobuild.sh
+++ apps/framework-cli/deploy/pseudobuild.sh
@@ -1,3 +1,8 @@
+if [ -z "$1" ]
+then
+      echo "You must specify the dockerhub repository as an argument. Example: ./pseudobuild.sh yourdockerhubusername"
+      exit 1
+fi
 cd deployment
 docker buildx create --use
 docker buildx build --platform=linux/amd64,linux/arm64 --push --no-cache -t $1/moose-deployment:latest .

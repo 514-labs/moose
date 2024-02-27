7b1091e5c3ec5c3f0402a454d3eb3440a3223fd8
cjus <carlos.justiniano@gmail.com>
2024-02-27 09:16:03 -0700

    moose packaging with mininal container

--- apps/framework-cli/deploy/pseudobuild.sh
+++ apps/framework-cli/deploy/pseudobuild.sh
@@ -4,4 +4,23 @@ then
       exit 1
 fi
 cd deployment
-docker buildx build --platform=linux/amd64,linux/arm64 --push --no-cache -t $1/moose-deployment:latest .
+
+VERSION="0.3.93"
+output=$(uname -a)
+os=$(echo $output | cut -d ' ' -f1)
+
+if [[ $os == "Linux" ]]; then
+    if [[ $output == *"x86_64"* ]]; then
+        docker buildx build --build-arg DOWNLOAD_URL=https://github.com/514-labs/moose/releases/download/v${VERSION}/moose-cli-x86_64-unknown-linux-gnu --platform=linux/amd64 --push --no-cache -t $1/moose-deployment:latest .
+    elif [[ $output == *"arm64"* ]]; then
+        docker buildx build --build-arg DOWNLOAD_URL=https://github.com/514-labs/moose/releases/download/v${VERSION}/moose-cli-aarch64-unknown-linux-gnu --platform=linux/arm64 --push --no-cache -t $1/moose-deployment:latest .
+    fi
+elif [[ $os == "Darwin" ]]; then
+    # Docker doesn't support creating macOS (darwin) containers as it uses Linux kernel features. 
+    # Therefore, there is no `--platform` value for creating darwin x86_64 and aarch64 Docker images. 
+    if [[ $output == *"x86_64"* ]]; then
+        docker buildx build --build-arg DOWNLOAD_URL=https://github.com/514-labs/moose/releases/download/v${VERSION}/moose-cli-x86_64-unknown-linux-gnu --platform=linux/amd64 --push --no-cache -t $1/moose-deployment:latest .
+    elif [[ $output == *"arm64"* ]]; then
+        docker buildx build --build-arg DOWNLOAD_URL=https://github.com/514-labs/moose/releases/download/v${VERSION}/moose-cli-aarch64-unknown-linux-gnu --platform=linux/arm64 --push --no-cache -t $1/moose-deployment:latest .
+    fi
+fi

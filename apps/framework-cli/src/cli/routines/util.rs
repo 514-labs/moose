0fe1327e856c9fd5dcdc31063d6907830f7c87c3
Jonathan Widjaja <smileforwidjaja@gmail.com>
2024-02-29 12:45:21 -0700

    Add log lines in ensure_docker_running for ci

--- apps/framework-cli/src/cli/routines/util.rs
+++ apps/framework-cli/src/cli/routines/util.rs
@@ -1,9 +1,12 @@
+use log::info;
+
 use crate::cli::display::Message;
 use crate::cli::routines::RoutineFailure;
 use crate::utilities::docker;
 
 pub fn ensure_docker_running() -> Result<(), RoutineFailure> {
     let errors = docker::check_status().map_err(|err| {
+        info!("Failed to run docker info");
         RoutineFailure::new(
             Message::new("Failed".to_string(), "to run `docker info`".to_string()),
             err,
@@ -11,16 +14,19 @@ pub fn ensure_docker_running() -> Result<(), RoutineFailure> {
     })?;
 
     if errors.is_empty() {
+        info!("Docker is running");
         Ok(())
     } else if errors
         .iter()
         .any(|s| s.ends_with("Is the docker daemon running?"))
     {
+        info!("Failed to run docker commands. Is docker running?");
         Err(RoutineFailure::error(Message::new(
             "Failed".to_string(),
             "to run docker commands. Is docker running?".to_string(),
         )))
     } else {
+        info!("Failed {}", errors.join("\n"));
         Err(RoutineFailure::error(Message::new(
             "Failed".to_string(),
             errors.join("\n"),

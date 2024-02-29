a7c56f9a99684bb0f3a2ecfeafdce26b6412a1e6
Jonathan Widjaja <smileforwidjaja@gmail.com>
2024-02-29 11:10:35 -0700

    validate .moose/models dir before starting dev

---
+++ apps/framework-cli/tests/cli_clean.rs
@@ -0,0 +1,49 @@
+use assert_cmd::prelude::*;
+use assert_fs::{prelude::*, TempDir};
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn can_run_cli_clean() -> Result<(), Box<dyn std::error::Error>> {
+    let temp = assert_fs::TempDir::new().unwrap();
+    let dir: &str = temp.path().to_str().unwrap();
+
+    temp.child(".moose").assert(predicate::path::missing());
+
+    let mut init_cmd = Command::cargo_bin("moose-cli")?;
+
+    init_cmd
+        .env("MOOSE-FEATURES-COMING_SOON_WALL", "false")
+        .arg("init")
+        .arg("test-app")
+        .arg("ts")
+        .arg(dir);
+
+    init_cmd.assert().success();
+
+    validate_dotmoose_dir(&temp, true);
+
+    let mut clean_cmd = Command::cargo_bin("moose-cli")?;
+
+    clean_cmd
+        .env("MOOSE-FEATURES-COMING_SOON_WALL", "false")
+        .arg("clean")
+        .current_dir(temp.path());
+
+    clean_cmd.assert().success();
+
+    validate_dotmoose_dir(&temp, false);
+
+    Ok(())
+}
+
+fn validate_dotmoose_dir(temp: &TempDir, should_exist: bool) {
+    let assert_value = if should_exist {
+        predicate::path::exists()
+    } else {
+        predicate::path::missing()
+    };
+    temp.child(".moose/models/typescript").assert(assert_value);
+    temp.child(".moose/.clickhouse").assert(assert_value);
+    temp.child(".moose/.panda_house").assert(assert_value);
+}

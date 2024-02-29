2984af5d3a8f3520a8e072521809451f8db4945d
Tim Delisle <9058816+tg339@users.noreply.github.com>
2024-02-29 15:13:08 -0800

    adding intrumentation to moosejs website (#488)
    * adding intrumentation to moosejs website
    
    * prettier fixes

---
+++ apps/web/src/app/events/sendServerEvent.ts
@@ -0,0 +1,21 @@
+import { headers } from "next/headers";
+
+export const sendServerEvent = async (name: string, event: any) => {
+  const headersList = headers();
+  const host = headersList.get("host");
+  const referer = headersList.get("referer");
+
+  const env = process.env.NODE_ENV;
+
+  const scheme = env === "production" ? "https" : "http";
+  const url = `${scheme}://${host}/events/api`;
+
+  fetch(url, {
+    method: "POST",
+    headers: headersList,
+    body: JSON.stringify({
+      name,
+      event: { ...event, host, env, referer },
+    }),
+  });
+};

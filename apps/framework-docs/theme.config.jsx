d9ca9b168c4419c0cefddad7ce00120d19340d04
George Anderson <georgevanderson@gmail.com>
2024-03-07 18:08:00 -0800

    add favicon to docs (#502)
    Co-authored-by: George Anderson <georgeanderson@Georges-MacBook-Pro-2.local>

--- apps/framework-docs/theme.config.jsx
+++ apps/framework-docs/theme.config.jsx
@@ -1,26 +1,31 @@
 export default {
-    logo: <div>moosejs</div>,
-    project: {
-      link: 'https://github.com/514-labs/moose'
-    },
-    docsRepositoryBase: 'https://github.com/514-labs/moose/tree/main/apps/framework-docs',
-    useNextSeoProps() {
-        return {
-          titleTemplate: '%s – MooseJS'
-        }
-      },
-      primaryHue: 220,
-      primarySaturation: 0,
-      footer: {
-        text: (
-          <span>
-            MIT | {new Date().getFullYear()} ©{' '} 
-            <a href="https://fiveonefour.com" target="_blank">
-                Fiveonefour Labs Inc
-            </a>
-            .
-          </span>
-        )
-      }
+  logo: <div>moosejs</div>,
+  project: {
+    link: 'https://github.com/514-labs/moose'
+  },
+  docsRepositoryBase: 'https://github.com/514-labs/moose/tree/main/apps/framework-docs',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – MooseJS'
+    }
+  },
+  head: () => (
+    <>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="16x16" />
+    </>
+  ),
+  primaryHue: 220,
+  primarySaturation: 0,
+  footer: {
+    text: (
+      <span>
+        MIT | {new Date().getFullYear()} ©{' '}
+        <a href="https://fiveonefour.com" target="_blank">
+          Fiveonefour Labs Inc
+        </a>
+        .
+      </span>
+    )
   }
+}
 

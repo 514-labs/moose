155b27ebe89d7bf7cb00de361e588dc96a5e73b4
cjus <carlos.justiniano@gmail.com>
2024-03-15 17:49:35 -0600

    update to allow for default or supplied endpoint URL

--- apps/framework-cli/deploy/senddata.sh
+++ apps/framework-cli/deploy/senddata.sh
@@ -4,7 +4,14 @@ eventID=$(LC_ALL=C < /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 userID=$(LC_ALL=C < /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
 currentTimestamp=$(date '+%Y-%m-%d %H:%M:%S')
 
+endpoint=$1
+if [ -z "$1" ]
+then
+    endpoint="http://localhost:4000/ingest/UserActivity"
+fi
+
 curl -v -X POST \
 -H "Content-Type: application/json" \
 -d "{\"eventId\": \"$eventID\", \"timestamp\": \"$currentTimestamp\", \"userId\": \"$userID\", \"activityType\": \"click\"}" \
-http://localhost:4000/ingest/UserActivity
+$endpoint
+

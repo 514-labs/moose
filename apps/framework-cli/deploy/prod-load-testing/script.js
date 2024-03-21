31457bca7c5094fbcee2a3c9bf53b102f7eeadaa
Carlos Justiniano <carlos.justiniano@gmail.com>
2024-03-20 21:13:43 -0600

    Updated eventId to represent JS uix timestamp and userId to be a random string. (#646)

--- apps/framework-cli/deploy/prod-load-testing/script.js
+++ apps/framework-cli/deploy/prod-load-testing/script.js
@@ -1,30 +1,27 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
+import http from "k6/http";
+import { check, sleep } from "k6";
 
-// The function that defines VU logic.
-//
-// See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
-// about authoring k6 scripts.
-//
-export default function() {
-  const url = 'http://34.82.14.129:4000/ingest/UserActivity';
-  const eventId = "loadtest-run-01";
-  const userId = "3434";
-  
+export default function () {
+  const url = "http://34.82.14.129:4000/ingest/UserActivity";
+  const eventId = Math.floor(new Date().getTime());
+  const userId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(
+    36
+  );
+
   const payload = JSON.stringify({
     eventId,
     userId,
     timestamp: new Date().toISOString(),
-    activityType: "loadtest"
+    activity: "loadtest",
   });
   const params = {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   };
 
-  let res = http.post(url, payload, params);  
-  check(res, { 'status was 200': (r) => r.status == 200 });
+  let res = http.post(url, payload, params);
+  check(res, { "status was 200": (r) => r.status == 200 });
 
   sleep(0.25);
 }

431e26e1d7ad0c9f03c56dd7404e5c3293444af8
Tim Delisle <tim@fiveonefour.com>
2024-03-22 14:30:34 -0700

    refactor email section

--- apps/framework-landing/app/sections/EmailSection.tsx
+++ apps/framework-landing/app/sections/EmailSection.tsx
@@ -8,11 +8,15 @@ import {
 import { SuperDisplay } from "@/components/typography/standard";
 
 export const EmailSection = () => {
+  const content = {
+    email: "hello@moosejs.dev",
+  };
+
   return (
     <Section className="text-center mb-0 lg:mb-0 2xl:mb-0 ">
       <Grid>
         <FullWidthContentContainer>
-          <SuperDisplay> hello@moosejs.dev </SuperDisplay>
+          <SuperDisplay> {content.email} </SuperDisplay>
         </FullWidthContentContainer>
       </Grid>
     </Section>

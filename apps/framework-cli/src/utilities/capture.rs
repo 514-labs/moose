161d2cf4c2460087a9c6b475abaf8bd9c6e3a6d1
Jonathan Widjaja <DatGuyJonathan@users.noreply.github.com>
2024-03-28 16:35:28 -0600

    Update instrumentation url & data model (#740)

--- apps/framework-cli/src/utilities/capture.rs
+++ apps/framework-cli/src/utilities/capture.rs
@@ -17,26 +17,26 @@ use uuid::Uuid;
 
 #[derive(Debug, Clone, Serialize)]
 pub enum ActivityType {
-    #[serde(rename = "devCommand")]
-    DevCommand,
-    #[serde(rename = "initCommand")]
-    InitCommand,
     #[serde(rename = "buildCommand")]
     BuildCommand,
-    #[serde(rename = "dockerCommand")]
-    DockerCommand,
-    #[serde(rename = "cleanCommand")]
-    CleanCommand,
-    #[serde(rename = "stopCommand")]
-    StopCommand,
-    #[serde(rename = "prodCommand")]
-    ProdCommand,
     #[serde(rename = "bumpVersionCommand")]
     BumpVersionCommand,
+    #[serde(rename = "cleanCommand")]
+    CleanCommand,
+    #[serde(rename = "devCommand")]
+    DevCommand,
+    #[serde(rename = "dockerCommand")]
+    DockerCommand,
+    #[serde(rename = "initCommand")]
+    InitCommand,
+    #[serde(rename = "prodCommand")]
+    ProdCommand,
+    #[serde(rename = "stopCommand")]
+    StopCommand,
 }
 
 #[derive(Debug, Clone, Serialize)]
-pub struct UserActivity {
+pub struct MooseActivity {
     pub id: Uuid,
     pub project: String,
     #[serde(rename = "activityType")]
@@ -52,15 +52,15 @@ pub struct UserActivity {
 macro_rules! capture {
     ($activity_type:expr, $sequence_id:expr, $project_name:expr) => {
         use crate::project::PROJECT;
-        use crate::utilities::capture::{ActivityType, UserActivity};
+        use crate::utilities::capture::{ActivityType, MooseActivity};
         use crate::utilities::constants;
         use chrono::Utc;
-        // use reqwest::Client;
+        use reqwest::Client;
         use serde_json::json;
         use uuid::Uuid;
 
         #[allow(unused)]
-        let event = &json!(UserActivity {
+        let event = json!(MooseActivity {
             id: Uuid::new_v4(),
             project: $project_name,
             activity_type: $activity_type,
@@ -68,23 +68,28 @@ macro_rules! capture {
             timestamp: Utc::now(),
             cli_version: constants::CLI_VERSION.to_string(),
         });
-
         let remote_url = {
             let guard = PROJECT.lock().unwrap();
             guard.instrumentation_config.url().clone()
         };
-        #[allow(unused)]
-        let prod_url = format!("{}/ingest/UserActivity", remote_url);
 
-        // let client = Client::new();
-        // let res = client
-        //     .post(&dev_url)
-        //     .json(event)
-        //     .send()
-        //     .await
-        //     .unwrap();
+        // Sending this data can fail for a variety of reasons, so we don't want to
+        // block user & no need to handle the result
+        tokio::spawn(async move {
+            // TODO: Change this to MooseActivity after the table is verified
+            let instrumentation_url = format!("{}/ingest/UserActivity", remote_url);
+            // TODO: Delete this, use event from above instead
+            let fake_event = json!({
+                "eventId": "1234",
+                "userId": "123456",
+                "activity": $activity_type,
+                "timestamp": Utc::now(),
+            });
 
-        // println!("Sent to {}. Event: {}", prod_url, event);
+            let client = Client::new();
+            let request = client.post(&instrumentation_url).json(&fake_event);
+            request.send().await
+        });
     };
 }
 

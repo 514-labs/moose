12b55bde9dc282dd36e647d524b4cb9050f98717
Tim Delisle <tim@fiveonefour.com>
2024-03-29 07:08:51 -0700

    prettier run

--- apps/company-landing/app/layout.tsx
+++ apps/company-landing/app/layout.tsx
@@ -47,7 +47,7 @@ export default async function RootLayout({
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           monoFont.variable,
-          sansFont.variable
+          sansFont.variable,
         )}
         suppressHydrationWarning
       >

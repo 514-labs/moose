e3720d0965a66bcf090e0da48f5d5407f5fc9134
Alex <alex.lakas@gmail.com>
2024-04-09 11:59:42 -0700

    Polish v1 items

--- apps/company-landing/app/sections/EmailSection.tsx
+++ apps/company-landing/app/sections/EmailSection.tsx
@@ -9,7 +9,7 @@ import { SuperDisplay } from "design-system/typography";
 
 export const EmailSection = () => {
   const content = {
-    email: "hello@moosejs.dev",
+    email: "hello@fiveonefour.com",
   };
 
   return (

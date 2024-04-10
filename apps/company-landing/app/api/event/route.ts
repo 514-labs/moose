011b9846953b5e52db974fee622380706598c866
George Anderson <georgeanderson@Georges-MBP-2.localdomain>
2024-04-10 16:13:36 -0700

    remove await

--- apps/company-landing/app/api/event/route.ts
+++ apps/company-landing/app/api/event/route.ts
@@ -4,6 +4,6 @@ import { sendServerEvent } from "event-capture/server-event";
 // send an event to mixpanel
 export async function POST(request: NextRequest) {
   const { name, ...payload } = await request.json();
-  await sendServerEvent(name, payload);
+  sendServerEvent(name, payload);
   return new Response("OK");
 }

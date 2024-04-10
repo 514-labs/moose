03b87a99e62d7e190c1ef0211a6178b713e14435
George Anderson <georgeanderson@Georges-MBP-2.localdomain>
2024-04-10 16:29:35 -0700

    exclude sentry

--- apps/framework-docs/src/middleware.ts
+++ apps/framework-docs/src/middleware.ts
@@ -10,7 +10,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|_next/static|images|favicon.ico).*)",
+    "/((?!monitoring-tunnel|api|_next/static|images|favicon.ico).*)",
   ],
 };
 export async function middleware(request: NextRequest) {

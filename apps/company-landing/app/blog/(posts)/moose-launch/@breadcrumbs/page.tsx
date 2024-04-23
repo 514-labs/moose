a491a279fc4228a24047cbc43924ca2a6bf10343
Tim Delisle <tim@fiveonefour.com>
2024-04-23 15:39:56 -0700

    added breadcrumbs

---
+++ apps/company-landing/app/blog/(posts)/moose-launch/@breadcrumbs/page.tsx
@@ -0,0 +1,22 @@
+import { Text } from "design-system/typography";
+import { humanReadableDate } from "../../../../../lib/formatter";
+
+const getMetaFromParent = async () => {
+  const { metadata } = await import(`../page.mdx`);
+  return metadata;
+};
+
+export default async function Breadcrumbs() {
+  const meta = await getMetaFromParent();
+
+  return (
+    <div>
+      <Text className="mb-0">
+        <a href="/blog" className=" text-muted-foreground">
+          Blog /
+        </a>{" "}
+        <a href="">{meta.title}</a>
+      </Text>
+    </div>
+  );
+}

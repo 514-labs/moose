f988337208bc9a7248b5bcc24b8eb0777ded8d3e
Nicolas Joseph <nicolas@fiveonefour.com>
2024-04-24 11:54:07 -0700

    add support for arrays in the console

--- apps/framework-cli/src/framework/schema.rs
+++ apps/framework-cli/src/framework/schema.rs
@@ -184,7 +184,7 @@ pub enum ColumnDefaults {
     Now,
 }
 
-#[derive(Debug, Clone, Serialize, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum ColumnType {
     String,
     Boolean,
@@ -201,7 +201,43 @@ pub enum ColumnType {
 
 impl fmt::Display for ColumnType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
+        match self {
+            ColumnType::String => write!(f, "String"),
+            ColumnType::Boolean => write!(f, "Boolean"),
+            ColumnType::Int => write!(f, "Int"),
+            ColumnType::BigInt => write!(f, "BigInt"),
+            ColumnType::Float => write!(f, "Float"),
+            ColumnType::Decimal => write!(f, "Decimal"),
+            ColumnType::DateTime => write!(f, "DateTime"),
+            ColumnType::Enum(e) => write!(f, "Enum<{}>", e.name),
+            ColumnType::Array(inner) => write!(f, "Array<{}>", inner),
+            ColumnType::Json => write!(f, "Json"),
+            ColumnType::Bytes => write!(f, "Bytes"),
+        }
+    }
+}
+
+impl Serialize for ColumnType {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        match self {
+            ColumnType::String => serializer.serialize_str("String"),
+            ColumnType::Boolean => serializer.serialize_str("Boolean"),
+            ColumnType::Int => serializer.serialize_str("Int"),
+            ColumnType::BigInt => serializer.serialize_str("BigInt"),
+            ColumnType::Float => serializer.serialize_str("Float"),
+            ColumnType::Decimal => serializer.serialize_str("Decimal"),
+            ColumnType::DateTime => serializer.serialize_str("DateTime"),
+            ColumnType::Enum(_) => {
+                let serial = format!("{}", self);
+                serializer.serialize_str(&serial)
+            }
+            ColumnType::Array(_) => {
+                let serial = format!("{}", self);
+                serializer.serialize_str(&serial)
+            }
+            ColumnType::Json => serializer.serialize_str("Json"),
+            ColumnType::Bytes => serializer.serialize_str("Bytes"),
+        }
     }
 }
 

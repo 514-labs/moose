bf6183bb3c5d88fe6e0da755ab1cd1f0078a9f85
Alex <alex.lakas@gmail.com>
2024-04-24 21:29:46 -0700

    Added footer to blog
    Added it to both parent and child page but for some reason it doesnt show up on child page.

--- apps/company-landing/app/blog/page.tsx
+++ apps/company-landing/app/blog/page.tsx
@@ -1,15 +1,25 @@
+import {
+  Section,
+  Grid,
+  HalfWidthContentContainer,
+} from "design-system/components/containers";
 import { Posts } from "./posts-list";
 import { getPosts } from "../../lib/posts";
-import { Section } from "design-system/components/containers";
 import { Display } from "design-system/typography";
+import FooterSection from "../sections/FooterSection";
+import { EmailSection } from "../sections/EmailSection";
 
 export default async function Blog() {
   const posts = await getPosts();
 
   return (
-    <Section>
-      <Display>Blog</Display>
-      <Posts posts={posts} />
-    </Section>
+    <>
+      <Section>
+        <Display>Blog</Display>
+        <Posts posts={posts} />
+      </Section>
+      <FooterSection />
+      <EmailSection />
+    </>
   );
 }
--- apps/company-landing/app/blog/posts-list.tsx
+++ apps/company-landing/app/blog/posts-list.tsx
@@ -3,32 +3,39 @@ import {
   Grid,
   HalfWidthContentContainer,
 } from "design-system/components/containers";
+import React from "react";
 import { type Post } from "../../lib/posts";
 import Link from "next/link";
 import { Heading, Text } from "design-system/typography";
 import { humanReadableDate } from "../../lib/formatter";
+import FooterSection from "../sections/FooterSection";
+import { EmailSection } from "../sections/EmailSection";
 
 export function Posts({ posts }: { posts: Post[] }) {
   return (
-    <Section className="px-0">
-      <Grid>
-        {posts.map(({ slug, title, description, publishedAt, categories }) => (
-          <>
-            <HalfWidthContentContainer>
-              <Text className="mb-0">{humanReadableDate(publishedAt)}</Text>
-              <Text className="mt-0">
-                {categories.map((cat, i) => `${i ? ", " : ""}${cat}`)}
-              </Text>
-            </HalfWidthContentContainer>
-            <HalfWidthContentContainer>
-              <Link href={`blog/${slug}`}>
-                <Heading>{title}</Heading>
-                <Text>{description}</Text>
-              </Link>
-            </HalfWidthContentContainer>
-          </>
-        ))}
-      </Grid>
-    </Section>
+    <>
+      <Section className="px-0">
+        <Grid>
+          {posts.map(
+            ({ slug, title, description, publishedAt, categories }) => (
+              <React.Fragment key={slug}>
+                <HalfWidthContentContainer>
+                  <Text className="mb-0">{humanReadableDate(publishedAt)}</Text>
+                  <Text className="mt-0">
+                    {categories.map((cat, i) => `${i ? ", " : ""}${cat}`)}
+                  </Text>
+                </HalfWidthContentContainer>
+                <HalfWidthContentContainer>
+                  <Link href={`blog/${slug}`}>
+                    <Heading>{title}</Heading>
+                    <Text>{description}</Text>
+                  </Link>
+                </HalfWidthContentContainer>
+              </React.Fragment>
+            ),
+          )}
+        </Grid>
+      </Section>
+    </>
   );
 }

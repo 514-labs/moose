6359c6f691f4fe32be87a8429a5514247f2e7fcd
George Anderson <georgevanderson@gmail.com>
2024-05-03 09:10:29 -0700

    fix bullets (#1121)
    Co-authored-by: George Anderson <georgevanders@gmail.com>

--- apps/framework-docs/theme.config.jsx
+++ apps/framework-docs/theme.config.jsx
@@ -1,19 +1,33 @@
-import {Display, Heading, Text, TextEmbed, HeadingLevel, SmallText, SmallTextEmbed} from "design-system/typography";
-import {Logo, Badge} from "design-system/components";``
+import {
+  Display,
+  Heading,
+  Text,
+  TextEmbed,
+  HeadingLevel,
+  SmallText,
+  SmallTextEmbed,
+} from "design-system/typography";
+import { Logo, Badge } from "design-system/components";
+``;
 
 export default {
-  logo: () => <div className="flex flex-row"><Logo property="moosejs" subProperty="docs" />
-  <Badge className="ml-3 mt-1.5" variant={"outline"}>
-                      alpha
-                    </Badge></div>,
+  logo: () => (
+    <div className="flex flex-row">
+      <Logo property="moosejs" subProperty="docs" />
+      <Badge className="ml-3 mt-1.5" variant={"outline"}>
+        alpha
+      </Badge>
+    </div>
+  ),
   project: {
-    link: 'https://github.com/514-labs/moose'
+    link: "https://github.com/514-labs/moose",
   },
-  docsRepositoryBase: 'https://github.com/514-labs/moose/tree/main/apps/framework-docs',
+  docsRepositoryBase:
+    "https://github.com/514-labs/moose/tree/main/apps/framework-docs",
   useNextSeoProps() {
     return {
-      titleTemplate: '%s – MooseJS'
-    }
+      titleTemplate: "%s – MooseJS",
+    };
   },
   head: () => (
     <>
@@ -22,49 +36,59 @@ export default {
   ),
 
   components: {
-    h1: ({children}) => <Heading >{children}</Heading>,
-    h2: ({children}) => <Heading longForm level={HeadingLevel.l2}>{children}</Heading>,
-    h3: ({children}) => <Heading longForm level={HeadingLevel.l3}>{children}</Heading>,
-    h4: ({children}) => <Heading longForm level={HeadingLevel.l4}>{children}</Heading>,
-    p: ({children}) => <SmallText>{children}</SmallText>,
-    ul: ({children}) => <SmallTextEmbed>{children}</SmallTextEmbed>,
-    ol: ({children}) => <SmallTextEmbed>{children}</SmallTextEmbed>
+    h1: ({ children }) => <Heading>{children}</Heading>,
+    h2: ({ children }) => (
+      <Heading longForm level={HeadingLevel.l2}>
+        {children}
+      </Heading>
+    ),
+    h3: ({ children }) => (
+      <Heading longForm level={HeadingLevel.l3}>
+        {children}
+      </Heading>
+    ),
+    h4: ({ children }) => (
+      <Heading longForm level={HeadingLevel.l4}>
+        {children}
+      </Heading>
+    ),
+    p: ({ children }) => <SmallText>{children}</SmallText>,
+    li: ({ children }) => (
+      <li>
+        <SmallTextEmbed>{children}</SmallTextEmbed>
+      </li>
+    ),
+    ul: ({ children }) => <SmallTextEmbed>{children}</SmallTextEmbed>,
+    ol: ({ children }) => <SmallTextEmbed>{children}</SmallTextEmbed>,
   },
   primaryHue: 220,
   primarySaturation: 0,
   sidebar: {
-    titleComponent({title}) {
+    titleComponent({ title }) {
       return (
-        <SmallText className="my-0 text-muted-foreground">
-          {title}
-        </SmallText>
-      )
-    }
+        <SmallText className="my-0 text-muted-foreground">{title}</SmallText>
+      );
+    },
   },
   toc: {
     title: () => {
-      return (
-        <SmallTextEmbed> On this page </SmallTextEmbed>
-      )
+      return <SmallTextEmbed> On this page </SmallTextEmbed>;
     },
-    headingComponent({children}) {
+    headingComponent({ children }) {
       return (
-        <SmallText className="my-0 text-muted-foreground">
-          {children}
-        </SmallText>
-      )
-    }
+        <SmallText className="my-0 text-muted-foreground">{children}</SmallText>
+      );
+    },
   },
   footer: {
     text: (
       <span>
-        MIT | {new Date().getFullYear()} ©{' '}
+        MIT | {new Date().getFullYear()} ©{" "}
         <a href="https://fiveonefour.com" target="_blank">
           Fiveonefour Labs Inc
         </a>
         .
       </span>
-    )
-  }
-}
-
+    ),
+  },
+};

ca36c114831108268e432abbad71cf4566876cbe
Olivia Kane <67128565+okane16@users.noreply.github.com>
2024-05-10 09:28:40 -0400

    product update 1 draft (#1214)
    * product update 1 draft
    
    * remove avatar import

---
+++ apps/company-landing/app/blog/(posts)/product-update-1/@meta/page.tsx
@@ -0,0 +1,32 @@
+import { Text } from "design-system/typography";
+import { humanReadableDate } from "../../../../../lib/formatter";
+
+const getMetaFromParent = async () => {
+  const { metadata } = await import(`../page.mdx`);
+  return metadata;
+};
+
+export default async function Meta() {
+  const meta = await getMetaFromParent();
+
+  return (
+    <div>
+      <Text className="mb-0">{humanReadableDate(meta.publishedAt)}</Text>
+      {meta.categories.map((cat: string, i: number) => {
+        if (i === meta.categories.length - 1) {
+          return (
+            <Text className="mt-0" key={i}>
+              {cat}
+            </Text>
+          );
+        } else {
+          return (
+            <Text className="mt-0" key={i}>
+              {cat},{" "}
+            </Text>
+          );
+        }
+      })}
+    </div>
+  );
+}

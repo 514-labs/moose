120ef85df15129e4b1d1f7c6bd8be77863263708
Carlos Justiniano <carlos.justiniano@gmail.com>
2024-05-14 15:27:04 -0600

    Fix for npm event dependency (#1234)

--- packages/design-system/scripts/release.sh
+++ packages/design-system/scripts/release.sh
@@ -12,6 +12,10 @@ jq \
     package.json > package.json.tmp \
     && mv package.json.tmp package.json
 
+jq '.dependencies["@514labs/event-capture"]="$version"' \
+    package.json > package.json.tmp \
+    && mv package.json.tmp package.json
+
 cd ../..
 pnpm build --filter=design-system
 cd packages/design-system

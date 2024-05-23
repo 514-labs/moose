51144e6f4294e1e91cbb01d6578dc938696ff4b8
George Anderson <georgevanders@gmail.com>
2024-05-23 13:09:23 -0700

    add key

--- apps/framework-landing/app/layout.tsx
+++ apps/framework-landing/app/layout.tsx
@@ -48,6 +48,7 @@ export default async function RootLayout({
         src="https://analytics.514.dev/script.js"
         data-host="https://moosefood.514.dev"
       />
+      <GoogleTagManager gtmId="AW-16560033470" />
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",

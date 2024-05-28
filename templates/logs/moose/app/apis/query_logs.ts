c68e3e892853abcecb2a7d9c91cc98b9771d6c21
George Leung <phiSgr@users.noreply.github.com>
2024-05-28 17:57:36 -0400

    otel demo (#1272)
    * stuff
    
    * somehow we have to convert it to number
    
    * flow that simplifies and flattens
    
    * update flows to latest versions and add aggregation query
    
    ---------
    
    Co-authored-by: Nicolas Joseph <nicolas@fiveonefour.com>

---
+++ templates/logs/moose/app/apis/query_logs.ts
@@ -0,0 +1,34 @@
+// Here is a sample api configuration that creates an API which serves the daily active users materialized view
+
+interface QueryParams {
+  startTime?: string;
+  endTime?: string;
+  logSubstring: string;
+}
+
+const subtractOneHour = (date: Date) => {
+  const dup = new Date(date);
+  dup.setHours(dup.getHours() - 1); // out of range is fine
+  return dup;
+};
+
+export default async function handle(
+  { startTime, endTime, logSubstring }: QueryParams,
+  { client, sql },
+) {
+  const end = endTime ? new Date(endTime) : new Date();
+  const start = startTime ? new Date(startTime) : subtractOneHour(end);
+
+  console.log("start", start, "end", end);
+
+  const pattern = `%${logSubstring}%`;
+
+  return client.query(
+    sql`SELECT * FROM Log
+      WHERE 
+        observedTimestamp >= ${start.getTime() / 1000} AND 
+        observedTimestamp < ${end.getTime() / 1000} AND
+        ilike(body, ${pattern})
+    `,
+  );
+}

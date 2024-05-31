77a97d275ec06a65691656e4c9ebf7d4371fbef1
Nicolas Joseph <nicolas@fiveonefour.com>
2024-05-31 12:52:16 -0700

    fix publishing script (#1341)

--- packages/ts-moose-lib/scripts/release-lib.sh
+++ packages/ts-moose-lib/scripts/release-lib.sh
@@ -8,6 +8,7 @@ version=$1
 
 cd ./packages/ts-moose-lib
 npm version $version --no-git-tag-version
+cd ../..
 
 # # This is run twice since the change the value of the dependencies in the previous step
 pnpm install --filter "@514labs/moose-lib" --no-frozen-lockfile # requires optional dependencies to be present in the registry

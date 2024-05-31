cd7f8add43c0e80b1357d335c378222e28e06f88
Nicolas Joseph <nicolas@fiveonefour.com>
2024-05-31 14:59:04 -0700

    chore: simplify dependencies for the user

--- apps/framework-cli/src/project/typescript_project.rs
+++ apps/framework-cli/src/project/typescript_project.rs
@@ -36,15 +36,7 @@ impl Default for TypescriptProject {
                 ("moose".to_string(), "moose-cli".to_string()),
                 ("build".to_string(), "moose-cli build --docker".to_string()),
             ]),
-            dependencies: HashMap::from([
-                ("@514labs/moose-lib".to_string(), "latest".to_string()),
-                ("@clickhouse/client-web".to_string(), "^1.0.1".to_string()),
-                ("fastq".to_string(), "^1.17.1".to_string()),
-                ("kafkajs".to_string(), "^2.2.4".to_string()),
-                // TODO: in production we should use the tsc compiler instead of ts-node
-                // For now though it is easier to use ts-node
-                ("ts-node".to_string(), "^10.9.2".to_string()),
-            ]),
+            dependencies: HashMap::from([("@514labs/moose-lib".to_string(), "latest".to_string())]),
             dev_dependencies: HashMap::from([
                 ("@514labs/moose-cli".to_string(), "latest".to_string()),
                 ("typescript".to_string(), "^5.4.0".to_string()),
--- packages/ts-moose-lib/src/index.ts
+++ packages/ts-moose-lib/src/index.ts
@@ -17,3 +17,18 @@ export interface ConsumptionUtil {
   // SQL interpolator
   sql: (strings: readonly string[], ...values: readonly (any | SQL)[]) => SQL;
 }
+
+export enum IngestionFormat {
+  JSON = "JSON",
+  JSON_ARRAY = "JSON_ARRAY",
+}
+
+export type DataModelConfig = Partial<{
+  ingestion: {
+    format?: IngestionFormat;
+  };
+  storage: {
+    enabled?: boolean;
+    order_by_fields?: string[];
+  };
+}>;

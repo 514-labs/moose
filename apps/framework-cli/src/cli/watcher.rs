f141446b72aa98e4c68c923d09dd5ecb4bf81a8b
Jonathan Widjaja <smileforwidjaja@gmail.com>
2024-06-24 16:31:36 -0600

    write state on file change

--- apps/framework-cli/src/cli/watcher.rs
+++ apps/framework-cli/src/cli/watcher.rs
@@ -28,6 +28,7 @@ use crate::framework::flows::loader::get_all_current_flows;
 use crate::framework::flows::registry::FlowProcessRegistry;
 use crate::framework::registry::model::ProcessRegistries;
 use crate::infrastructure::kafka_clickhouse_sync::SyncingProcessesRegistry;
+use crate::infrastructure::olap::clickhouse_alt_client::{get_pool, store_current_state};
 use crate::infrastructure::stream::redpanda;
 use crate::project::AggregationSet;
 use crate::utilities::constants::{AGGREGATIONS_DIR, CONSUMPTION_DIR, FLOWS_DIR, SCHEMAS_DIR};
@@ -338,6 +339,18 @@ async fn watch(
                 let bucketed_events = EventBuckets::new(events);
 
                 if bucketed_events.non_empty() {
+                    {
+                        let mut client = get_pool(&project.clickhouse_config).get_handle().await?;
+                        let aggregations = project.get_aggregations();
+                        store_current_state(
+                            &mut client,
+                            framework_object_versions,
+                            &aggregations,
+                            &project.clickhouse_config,
+                        )
+                        .await?
+                    }
+
                     if !bucketed_events.data_models.is_empty() {
                         with_spinner_async(
                             &format!(

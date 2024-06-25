88e7a12779ebfd1c44e4b93cc4308e2748371886
Aditya Chandrashekaran <aditya@Adityas-MacBook-Pro-2.local>
2024-06-25 14:03:02 -0700

    Removed Comments

--- apps/framework-internal-app/app/flows/Logs/ParsedLogs/flow.ts
+++ apps/framework-internal-app/app/flows/Logs/ParsedLogs/flow.ts
@@ -3,38 +3,6 @@ import { ParsedLogs, Logs } from "../../../datamodels/logs";
 export default function run(source: Logs): ParsedLogs[] {
   let returnArray: ParsedLogs[] = [];
 
-  //   for (let a = 0; a < source.resourceLogs.length; a++) {
-  //     for (let b = 0; b < source.resourceLogs[a].scopeLogs.length; b++) {
-  //       for (
-  //         let c = 0;
-  //         c < source.resourceLogs[a].scopeLogs[b].logRecords.length;
-  //         c++
-  //       ) {
-  //         let d = new Date(
-  //           Number(
-  //             source.resourceLogs[a].scopeLogs[b].logRecords[c]
-  //               .observedTimeUnixNano,
-  //           ) / 1000000,
-  //         );
-  //         returnArray.push({
-  //           date: d.toLocaleString(),
-  //           message:
-  //             source.resourceLogs[a].scopeLogs[b].logRecords[c].body.value
-  //               .stringValue,
-  //           severityNumber:
-  //             source.resourceLogs[a].scopeLogs[b].logRecords[c].severityNumber,
-  //           severityLevel:
-  //             source.resourceLogs[a].scopeLogs[b].logRecords[c].severityText,
-  //           source:
-  //             source.resourceLogs[a].scopeLogs[b].logRecords[c].attributes[0]
-  //               .value.stringValue,
-  //           sessionId: source.resourceLogs[a].resource.attributes[0].key,
-  //           serviceName: source.resourceLogs[a].resource.attributes[1].key,
-  //         });
-  //       }
-  //     }
-  //   }
-
   for (const resourceLog of source.resourceLogs) {
     for (const scopeLog of resourceLog.scopeLogs) {
       for (const logRecord of scopeLog.logRecords) {

e5a0acc62d0410ca5871edf98e2c68fb6b1756ef
Aditya Chandrashekaran <aditya@Adityas-MacBook-Pro-2.local>
2024-06-25 13:14:39 -0700

    Flow generates new object for every log message

--- apps/framework-internal-app/app/flows/Logs/ParsedLogs/flow.ts
+++ apps/framework-internal-app/app/flows/Logs/ParsedLogs/flow.ts
@@ -1,26 +1,39 @@
-// Example flow function: Converts local timestamps in UserActivity data to UTC.
-
-// Imports: Source (UserActivity) and Destination (ParsedActivity) data models.
 import { ParsedLogs, Logs } from "../../../datamodels/logs";
 
-// The 'run' function transforms UserActivity data to ParsedActivity format.
-// For more details on how Moose flows work, see: https://docs.moosejs.com
-export default function run(source: Logs): ParsedLogs {
-  // Convert local timestamp to UTC and return new ParsedActivity object.
-  return {
-    date: new Date(
-      source.resourceLogs[0].scopeLogs[0].logRecords[0].observedTimeUnixNano,
-    ),
-    message:
-      source.resourceLogs[0].scopeLogs[0].logRecords[0].body.value.stringValue,
-    severityNumber:
-      source.resourceLogs[0].scopeLogs[0].logRecords[0].severityNumber,
-    severityLevel:
-      source.resourceLogs[0].scopeLogs[0].logRecords[0].severityText,
-    source:
-      source.resourceLogs[0].scopeLogs[0].logRecords[0].attributes[0].value
-        .stringValue,
-    sessionId: source.resourceLogs[0].resource.attributes[0].key,
-    serviceName: source.resourceLogs[0].resource.attributes[1].key,
-  };
+export default function run(source: Logs): ParsedLogs[] {
+  let returnArray: ParsedLogs[] = [];
+
+  for (let a = 0; a < source.resourceLogs.length; a++) {
+    for (let b = 0; b < source.resourceLogs[a].scopeLogs.length; b++) {
+      for (
+        let c = 0;
+        c < source.resourceLogs[a].scopeLogs[b].logRecords.length;
+        c++
+      ) {
+        let d = new Date(
+          Number(
+            source.resourceLogs[a].scopeLogs[b].logRecords[c]
+              .observedTimeUnixNano,
+          ) / 1000000,
+        );
+        returnArray.push({
+          date: d.toLocaleString(),
+          message:
+            source.resourceLogs[a].scopeLogs[b].logRecords[c].body.value
+              .stringValue,
+          severityNumber:
+            source.resourceLogs[a].scopeLogs[b].logRecords[c].severityNumber,
+          severityLevel:
+            source.resourceLogs[a].scopeLogs[b].logRecords[c].severityText,
+          source:
+            source.resourceLogs[a].scopeLogs[b].logRecords[c].attributes[0]
+              .value.stringValue,
+          sessionId: source.resourceLogs[a].resource.attributes[0].key,
+          serviceName: source.resourceLogs[a].resource.attributes[1].key,
+        });
+      }
+    }
+  }
+
+  return returnArray;
 }

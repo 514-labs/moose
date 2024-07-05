52c8a8fe37115113746cbeae44ae9af29aefe9ac
George Anderson <georgevanders@gmail.com>
2024-07-05 09:30:43 -0600

    update

--- packages/design-system-base/tailwind.config.ts
+++ packages/design-system-base/tailwind.config.ts
@@ -74,8 +74,8 @@ const config = {
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "accordion-down": "accordion-down 0.5s ease-out",
+        "accordion-up": "accordion-up 0.5s ease-out",
       },
     },
   },

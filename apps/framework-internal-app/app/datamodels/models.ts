88cffdcf0af5a3bf73e6021941160f74bab5280f
Jonathan Widjaja <smileforwidjaja@gmail.com>
2024-07-09 11:38:25 -0700

    add bigger data model

--- apps/framework-internal-app/app/datamodels/models.ts
+++ apps/framework-internal-app/app/datamodels/models.ts
@@ -107,3 +107,75 @@ export interface UserActivityBatch {
   activity: string;
   description: string;
 }
+
+export const BigBatchConfig = {
+  ingestion: {
+    format: IngestionFormat.JSON_ARRAY,
+  },
+};
+
+export interface BigBatch {
+  id: Key<string>;
+  test_column_1: string;
+  test_column_2: string;
+  test_column_3: string;
+  test_column_4: string;
+  test_column_5: string;
+  test_column_6: string;
+  test_column_7: string;
+  test_column_8: string;
+  test_column_9: string;
+  test_column_10: string;
+  test_column_11: number;
+  test_column_12: number;
+  test_column_13: number;
+  test_column_14: number;
+  test_column_15: number;
+  test_column_16: number;
+  test_column_17: number;
+  test_column_18: number;
+  test_column_19: number;
+  test_column_20: number;
+  test_column_21: number;
+  test_column_22: number;
+  test_column_23: number;
+  test_column_24: number;
+  test_column_25: number;
+  test_column_26: number;
+  test_column_27: number;
+  test_column_28: number;
+  test_column_29: number;
+  test_column_30: number;
+  test_column_31: number;
+  test_column_32: number;
+  test_column_33: number;
+  test_column_34: number;
+  test_column_35: number;
+  test_column_36: number;
+  test_column_37: number;
+  test_column_38: number;
+  test_column_39: number;
+  test_column_40: number;
+  test_column_41: number;
+  test_column_42: number;
+  test_column_43: number;
+  test_column_44: number;
+  test_column_45: number;
+  test_column_46: number;
+  test_column_47: number;
+  test_column_48: number;
+  test_column_49: number;
+  test_column_50: number;
+  test_column_51: number;
+  test_column_52: number;
+  test_column_53: number;
+  test_column_54: number;
+  test_column_55: number;
+  test_column_56: number;
+  test_column_57: number;
+  test_column_58: number;
+  test_column_59: number;
+  test_column_60: number;
+  test_column_61: number;
+  test_column_62: number;
+}

8a07c4eeadf83a99a1f872bbba462cc7ca12d2af
George Anderson <georgevanders@gmail.com>
2024-07-09 12:05:38 -0700

    fix undefined check

--- apps/product-analytics/src/app/api/auth/[...nextauth]/route.ts
+++ apps/product-analytics/src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
       authorization: {
         params: {
           prompt: "consent",

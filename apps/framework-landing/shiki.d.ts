5ab46923d348ffcaeac69af4ef0f8f4685924dcd
Olivia Kane <olivia@fiveonefour.com>
2024-07-27 18:18:03 -0400

    remove shiki.d.ts

--- apps/framework-landing/shiki.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "shiki" {
-  export interface Highlighter {
-    codeToHtml(code: string, options: { lang: string }): Promise<string>;
-  }
-  export function getHighlighter(options: {
-    theme: string;
-  }): Promise<Highlighter>;
-}

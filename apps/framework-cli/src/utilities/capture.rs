928e8a646ba9c552c946f041c88246c5f7cadd46
Nicolas Joseph <nicolas@fiveonefour.com>
2024-07-29 17:26:42 -0700

    fix: short lived commands telemetry

--- apps/framework-cli/src/utilities/capture.rs
+++ apps/framework-cli/src/utilities/capture.rs
@@ -2,17 +2,23 @@
 //!
 //! This module leverages moose to instrument moose. It includes a macro to easily capture data anywhere in the codebase.
 //!
+use crate::cli::settings::Settings;
+use crate::utilities::constants;
+use crate::utilities::constants::{CONTEXT, CTX_SESSION_ID};
+use chrono::Utc;
 use lazy_static::lazy_static;
+use serde_json::json;
+use std::time::Duration;
+use uuid::Uuid;
 
 // Create a lazy static instance of the client
 lazy_static! {
     pub static ref CLIENT: reqwest::Client = reqwest::Client::new();
 }
 
-use chrono::{DateTime, Utc};
+use chrono::DateTime;
 
 use serde::Serialize;
-use uuid::Uuid;
 
 #[derive(Debug, Clone, Serialize)]
 pub enum ActivityType {
@@ -50,12 +56,22 @@ pub enum ActivityType {
     PsCommand,
     #[serde(rename = "stopCommand")]
     StopCommand,
+    #[serde(rename = "metricsCommand")]
+    MetricsCommand,
+    #[serde(rename = "importCommand")]
+    ImportCommand,
+    #[serde(rename = "datamodelInitCommand")]
+    DataModelInitCommand,
+    #[serde(rename = "generateSDKCommand")]
+    GenerateSDKCommand,
+    #[serde(rename = "generateMigrations")]
+    GenerateMigrations,
 }
 
 #[derive(Debug, Clone, Serialize)]
 pub struct MooseActivity {
     pub id: Uuid,
-    pub project: String,
+    pub project: Option<String>,
     #[serde(rename = "activityType")]
     pub activity_type: ActivityType,
     #[serde(rename = "sequenceId")]
@@ -71,60 +87,59 @@ pub struct MooseActivity {
     pub ip: Option<String>,
 }
 
-macro_rules! capture {
-    ($activity_type:expr, $project_name:expr, $settings:expr) => {
-        use crate::utilities::capture::{ActivityType, MooseActivity};
-        use crate::utilities::constants;
-        use crate::utilities::constants::{CONTEXT, CTX_SESSION_ID};
-        use chrono::Utc;
-        use reqwest::Client;
-        use serde_json::json;
-        use std::time::Duration;
-        use uuid::Uuid;
+pub fn capture_usage(
+    activity_type: ActivityType,
+    project_name: Option<String>,
+    settings: &Settings,
+) -> Option<tokio::task::JoinHandle<()>> {
+    // Ignore our deployments & internal testing
+    if settings.telemetry.enabled {
+        let mut event = MooseActivity {
+            id: Uuid::new_v4(),
+            project: project_name,
+            activity_type: activity_type,
+            sequence_id: CONTEXT.get(CTX_SESSION_ID).unwrap().clone(),
+            timestamp: Utc::now(),
+            cli_version: constants::CLI_VERSION.to_string(),
+            is_moose_developer: settings.telemetry.is_moose_developer,
+            machine_id: settings.telemetry.machine_id.clone(),
+            ip: None,
+        };
 
-        // Ignore our deployments & internal testing
-        if $settings.telemetry.enabled {
-            let client = Client::new();
-
-            let mut event = MooseActivity {
-                id: Uuid::new_v4(),
-                project: $project_name,
-                activity_type: $activity_type,
-                sequence_id: CONTEXT.get(CTX_SESSION_ID).unwrap().clone(),
-                timestamp: Utc::now(),
-                cli_version: constants::CLI_VERSION.to_string(),
-                is_moose_developer: $settings.telemetry.is_moose_developer,
-                machine_id: $settings.telemetry.machine_id.clone(),
-                ip: None,
-            };
-            tokio::task::spawn(async move {
-                match client
-                    .get("https://api64.ipify.org?format=text")
-                    .send()
-                    .await
-                {
-                    Ok(response) => {
-                        event.ip = Some(response.text().await.unwrap());
-                    }
-                    Err(e) => {
-                        log::warn!("Failed to get IP address for telemetry: {:?}", e);
-                    }
+        Some(tokio::task::spawn(async move {
+            match CLIENT
+                .get("https://api64.ipify.org?format=text")
+                .timeout(Duration::from_secs(2))
+                .send()
+                .await
+            {
+                Ok(response) => {
+                    event.ip = Some(response.text().await.unwrap());
                 }
-                let event = json!(event);
+                Err(e) => {
+                    log::warn!("Failed to get IP address for telemetry: {:?}", e);
+                }
+            }
+            let event = json!(event);
 
-                // Sending this data can fail for a variety of reasons, so we don't want to
-                // block user & no need to handle the result
-                // The API version is pinned on purpose to avoid breaking changes. We
-                // can deliberately update this when the schema changes.
-                let request = client
-                    .post("https://moosefood.514.dev/ingest/MooseActivity/0.4")
-                    .json(&event)
-                    .timeout(Duration::from_secs(2));
+            // Sending this data can fail for a variety of reasons, so we don't want to
+            // block user & no need to handle the result
+            // The API version is pinned on purpose to avoid breaking changes. We
+            // can deliberately update this when the schema changes.
+            let request = CLIENT
+                .post("https://moosefood.514.dev/ingest/MooseActivity/0.4")
+                .json(&event)
+                .timeout(Duration::from_secs(2));
 
-                let _ = request.send().await;
-            });
-        }
-    };
+            let _ = request.send().await;
+        }))
+    } else {
+        None
+    }
 }
 
-pub(crate) use capture;
+pub async fn wait_for_usage_capture(handle: Option<tokio::task::JoinHandle<()>>) {
+    if let Some(handle) = handle {
+        let _ = handle.await;
+    }
+}

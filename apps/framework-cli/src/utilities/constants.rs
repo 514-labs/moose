25c0a598a7d472ba61bd1a4916e57033dd50a5f6
George Leung <phiSgr@users.noreply.github.com>
2024-07-31 13:59:06 -0400

    stop creating app/aggregations directory and improve logging (#1639)

--- apps/framework-cli/src/utilities/constants.rs
+++ apps/framework-cli/src/utilities/constants.rs
@@ -35,12 +35,7 @@ pub const REDPANDA_CONTAINER_NAME: &str = "redpanda-1";
 pub const REDPANDA_HOSTS: [&str; 2] = ["redpanda", "localhost"];
 
 pub const APP_DIR: &str = "app";
-pub const APP_DIR_LAYOUT: [&str; 4] = [
-    SCHEMAS_DIR,
-    FUNCTIONS_DIR,
-    AGGREGATIONS_DIR,
-    CONSUMPTION_DIR,
-];
+pub const APP_DIR_LAYOUT: [&str; 4] = [SCHEMAS_DIR, FUNCTIONS_DIR, BLOCKS_DIR, CONSUMPTION_DIR];
 
 pub const GITIGNORE: &str = ".gitignore";
 

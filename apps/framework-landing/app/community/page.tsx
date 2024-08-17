b50c8579115162d38bb3c67eba1eb9a7c3ba6f8c
Alex <alex.lakas@gmail.com>
2024-08-16 17:06:37 -0700

    Community page figma parity

--- apps/framework-landing/app/community/page.tsx
+++ apps/framework-landing/app/community/page.tsx
@@ -2,17 +2,43 @@ import {
   Grid,
   HalfWidthContentContainer,
   Section,
+  ThirdWidthContentContainer,
+  FullWidthContentContainer,
 } from "@514labs/design-system-components/components/containers";
 import {
   Display,
   Heading,
   Text,
+  SmallText,
+  HeadingLevel,
 } from "@514labs/design-system-components/typography";
 import { CTABar } from "@514labs/design-system-components/components";
 import FooterSection from "../sections/FooterSection";
 import Link from "next/link";
 import { TrackCtaButton } from "../trackable-components";
-import { LooseMooseSection } from "../sections/home/LooseMooseSection";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import {
+  Box,
+  Network,
+  Share2,
+  Terminal,
+  Code2,
+  Server,
+  Github,
+  Slack,
+  MessageCircle,
+  TrendingDownIcon,
+  TerminalIcon,
+  TrendingUp,
+} from "lucide-react";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// import { cn } from "@514labs/design-system-components/lib/utils";
+// import { LooseMooseSection } from "../sections/home/LooseMooseSection";
 
 const content = {
   title: "Community",
@@ -54,23 +80,171 @@ const content = {
   ],
 };
 
+const HeroSection = ({ className }: { className?: string }) => {
+  const content = {
+    title: `Join The Dev Community`,
+    description:
+      "We believe that data is the ultimate bridge between the arts and the sciences. Our team brings together culture, creativity, and technology to build the future of data.",
+  };
+
+  return (
+    <section className={cn(className)}>
+      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl 2xl:text-7xl text-center z-10">
+        {content.title}
+      </div>
+      <div className="text-2xl 2xl:text-4xl text-center text-muted-foreground py-5 z-10">
+        {content.description}
+      </div>
+    </section>
+  );
+};
+
+const PickSection = ({ className }: { className?: string }) => {
+  const content = {
+    titleMuted:
+      "We're tired of clunky developer workflows when working with data-intensive apps. Boreal helps you",
+    titleHighlight: "get from your IDE to production in minutes.",
+  };
+
+  return <section className={className}>Pick your community</section>;
+};
+
+export const SkillSection = () => {
+  const content = {
+    skills: [
+      {
+        title: "Feedback",
+        description:
+          "We're in the early stages, and your feedback is like gold to us. We want to hear your thoughts, and suggestions to make our product great.",
+        icon: <MessageCircle strokeWidth={1} />,
+      },
+      {
+        title: "Insights",
+        description:
+          "We're eager to learn from your interactions with our product. Your insights help us uncover new possibilities and address any issues you encounter",
+        icon: <TrendingUp strokeWidth={1} />,
+      },
+      {
+        title: "Early Access",
+        description:
+          "Be among the first to try our product. Early access means you're not just a user; you're a co-creator. Together, we're building something amazing.",
+        icon: <TerminalIcon strokeWidth={1} />,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Section className="mx-auto max-w-5xl">
+        <Grid className="mb-0 2xl:mb-20">
+          <FullWidthContentContainer>
+            <Heading
+              level={HeadingLevel.l1}
+              className="max-w-5xl justify-center align-center text-center sm:text-5xl"
+            >
+              {content.title}
+            </Heading>
+            <Heading
+              level={HeadingLevel.l2}
+              className="max-w-5xl justify-center align-center text-center text-muted-foreground"
+            >
+              {content.subtitle}
+            </Heading>
+          </FullWidthContentContainer>
+        </Grid>
+        <Grid className="gap-y-10">
+          {content.skills.map((feature, index) => {
+            return (
+              <ThirdWidthContentContainer
+                key={index}
+                className="flex flex-col gap-5 border p-5 rounded-3xl"
+              >
+                {feature.icon}
+                <Text className="my-0">{feature.title}</Text>
+                <SmallText className="my-0 text-muted-foreground text-[20px]">
+                  {feature.description}
+                </SmallText>
+              </ThirdWidthContentContainer>
+            );
+          })}
+        </Grid>
+      </Section>
+    </>
+  );
+};
+
+export const SlackSection = () => {
+  const content = {
+    slack: [
+      {
+        title: "Slack",
+        description:
+          "A place for open product discussions, private groups, sharing feedback and getting access.",
+        icon: <Slack strokeWidth={1} />,
+      },
+      {
+        title: "Github",
+        description:
+          "Contribute to our open-source projects, report issues, and suggest new features.",
+        icon: <Github strokeWidth={1} />,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Section className="mx-auto max-w-5xl">
+        <Grid className="mb-0">
+          <FullWidthContentContainer>
+            <Heading
+              level={HeadingLevel.l1}
+              className="max-w-5xl justify-center align-center text-center sm:text-5xl"
+            >
+              {content.title}
+            </Heading>
+            <Heading
+              level={HeadingLevel.l2}
+              className="max-w-5xl justify-center align-center text-center text-muted-foreground"
+            >
+              {content.subtitle}
+            </Heading>
+          </FullWidthContentContainer>
+        </Grid>
+        <Grid className="gap-y-10">
+          {content.slack.map((feature, index) => {
+            return (
+              <HalfWidthContentContainer
+                key={index}
+                className="flex flex-col gap-5 border p-5 rounded-3xl"
+              >
+                {feature.icon}
+                <Text className="my-0">{feature.title}</Text>
+                <SmallText className="my-0 text-muted-foreground text-[20px]">
+                  {feature.description}
+                </SmallText>
+              </HalfWidthContentContainer>
+            );
+          })}
+        </Grid>
+      </Section>
+    </>
+  );
+};
+
 export default function Community() {
   return (
     <>
-      <Section className="px-6 w-full relative mx-auto xl:max-w-screen-xl">
+      {/* <Section className="px-6 w-full relative max-w-5xl mx-auto px-5 text-center ">
         <Display>Community</Display>
-      </Section>
-      <Section className="px-6 w-full relative mx-auto xl:max-w-screen-xl">
-        <Grid>
-          <HalfWidthContentContainer>
-            <Heading>{content.description.headline}</Heading>
-          </HalfWidthContentContainer>
-          <HalfWidthContentContainer>
-            <Text>{content.description.body}</Text>
-          </HalfWidthContentContainer>
-        </Grid>
-      </Section>
-      <Section className="px-6 w-full relative mx-auto xl:max-w-screen-xl">
+      </Section> */}
+      <HeroSection className="max-w-5xl mx-auto px-10 py-5 p-20 lg:py-0 flex flex-col items-center my-16 sm:my-40" />
+      <SkillSection />
+      <PickSection className="max-w-5xl mx-auto p-10 text-center text-3xl sm:text-5xl pb-0 mb-0" />
+      <SlackSection />
+      <FooterSection />
+
+      {/*       
+      <Section className="px-6 w-full relative">
         {content.reasons.map((reason, i) => (
           <Grid key={i} className="py-5 md:py-0">
             <HalfWidthContentContainer>
@@ -83,7 +257,7 @@ export default function Community() {
           </Grid>
         ))}
       </Section>
-      <Section className="px-6 w-full relative mx-auto xl:max-w-screen-xl">
+      <Section className="px-6 w-full relative max-w-5xl mx-auto px-5 text-center">
         <Heading>Pick your community</Heading>
       </Section>
       <Section className="px-6 w-full relative mx-auto xl:max-w-screen-xl">
@@ -109,10 +283,9 @@ export default function Community() {
             </HalfWidthContentContainer>
           </Grid>
         ))}
-      </Section>
-      <FooterSection />
+      </Section> */}
       {/* <EmailSection /> */}
-      <LooseMooseSection />
+      {/* <LooseMooseSection /> */}
     </>
   );
 }

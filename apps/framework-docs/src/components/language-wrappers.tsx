7a7b2a719a8823cf59ef74d75f31e6d3881a1e55
Olivia Kane <olivia@fiveonefour.com>
2024-09-13 14:29:28 -0400

    try with cn

--- apps/framework-docs/src/components/language-wrappers.tsx
+++ apps/framework-docs/src/components/language-wrappers.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import { useLanguage } from "./LanguageContext";
+import { cn } from "@514labs/design-system-components/utils";
 
 interface LanguageProps {
   children: ReactNode;
@@ -7,12 +8,20 @@ interface LanguageProps {
 
 export const TypeScript: React.FC<LanguageProps> = ({ children }) => {
   const { language } = useLanguage();
-  return language === "typescript" ? <>{children}</> : null;
+  return (
+    <div className={cn(language === "typescript" ? "" : "hidden")}>
+      {children}
+    </div>
+  );
+  //return language === "typescript" ? <>{children}</> : null;
 };
 
 export const Python: React.FC<LanguageProps> = ({ children }) => {
   const { language } = useLanguage();
-  return language === "python" ? <>{children}</> : null;
+  return (
+    <div className={cn(language === "python" ? "" : "hidden")}>{children}</div>
+  );
+  //return language === "python" ? <>{children}</> : null;
 };
 
 export const LanguageSwitch: React.FC<{

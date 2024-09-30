9f741d1f0dce617a3c6f96d0b31af7fabe68228b
Jonathan Widjaja <DatGuyJonathan@users.noreply.github.com>
2024-09-30 12:03:02 -0600

    validate jwt on ingest (#1785)

---
+++ apps/framework-cli/src/utilities/auth.rs
@@ -0,0 +1,23 @@
+use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Claims {
+    iss: String,
+    aud: String,
+    exp: i64,
+}
+
+pub fn validate_jwt(token: Option<&str>, public_key: &str, issuer: &str, audience: &str) -> bool {
+    token.map_or(false, |t| {
+        let mut validation = Validation::new(Algorithm::RS256);
+        validation.set_issuer(&[issuer]);
+        validation.set_audience(&[audience]);
+        validation.set_required_spec_claims(&["iss", "aud", "exp"]);
+
+        match DecodingKey::from_rsa_pem(public_key.as_ref()) {
+            Ok(decoding_key) => decode::<Claims>(t, &decoding_key, &validation).is_ok(),
+            Err(_) => false,
+        }
+    })
+}

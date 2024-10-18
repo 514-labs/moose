fa4474bfaa4855a302b00e013224721d5fac3df6
George Leung <phisgr@gmail.com>
2024-10-18 14:09:32 -0700

    stuff

--- apps/framework-cli/build.rs
+++ apps/framework-cli/build.rs
@@ -7,9 +7,9 @@ fn main() -> Result<()> {
         // // Use `protoc-bin-vendored` bundled protoc command, optional.
         // .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
         // All inputs and imports from the inputs must reside in `includes` directories.
-        .includes(["../../packages/infra-proto"])
+        .includes(["../../packages/protobuf"])
         // Inputs must reside in some of include paths.
-        .input("../../packages/infra-proto/infrastructure_map.proto")
+        .input("../../packages/protobuf/infrastructure_map.proto")
         // Specify output directory relative to Cargo output directory.
         .out_dir("src/proto/")
         .run_from_script();

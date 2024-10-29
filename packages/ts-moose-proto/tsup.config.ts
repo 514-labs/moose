86f1f7c1a7a23bbdf6618820ffdb108a6c50266f
George Leung <phisgr@gmail.com>
2024-10-29 16:26:31 -0400

    yolo

---
+++ packages/ts-moose-proto/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["generated/*.ts"],
+  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  dts: true, // Generate declaration file (.d.ts)
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+});

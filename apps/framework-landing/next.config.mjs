a8fcd2c0f7506423726bd3b966ca0048b4a33d34
Nicolas Joseph <nicolas@fiveonefour.com>
2025-03-26 10:46:32 -0700

    chore: remove framework landing

--- apps/framework-landing/next.config.mjs
+++
@@ -1,7 +0,0 @@
-import { withNextVideo } from "next-video/process";
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["design-system"],
-};
-
-export default withNextVideo(nextConfig);

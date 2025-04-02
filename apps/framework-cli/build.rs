4a169a75a9f8e062c80ab90eef9e55c72e9fa800
George Anderson <george.anderson@fiveonefour.com>
2025-04-02 15:11:23 -0600

    change the build

--- apps/framework-cli/build.rs
+++ apps/framework-cli/build.rs
@@ -9,17 +9,9 @@ fn package_templates() -> Result<()> {
         .unwrap();
 
     let package_templates_dir = root_dir.join("scripts/package-templates.js");
-    let home_dir = std::env::current_dir().unwrap();
-    let log_dir = home_dir.join(".moose");
-
-    std::fs::create_dir_all(&log_dir)?;
-    let log_file = log_dir.join("build.log");
-    let log_output = std::fs::File::create(log_file)?;
 
     Command::new(&package_templates_dir)
         .current_dir(root_dir)
-        .stdout(log_output.try_clone()?)
-        .stderr(log_output)
         .status()?;
     Ok(())
 }

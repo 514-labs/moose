0f8b48eeec9af997361556d6615cb1ebb4b665b4
George Anderson <george.anderson@fiveonefour.com>
2025-04-02 16:10:29 -0600

    uncomment index

--- templates/ts/app/index.ts
+++ templates/ts/app/index.ts
@@ -1,5 +1,5 @@
 export * from "./ingest/models";
 
 export * from "./functions/process";
-// export * from "./apis/bar";
-// export * from "./views/views";
+export * from "./apis/bar";
+export * from "./views/views";

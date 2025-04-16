ff70e48b91f1def4e47d86eeb5f23744616b46cf
Nicolas Joseph <nicolas@fiveonefour.com>
2025-04-16 14:05:24 -0700

    chore: adding description files to python packagibng

--- packages/py-moose-lib/setup.py
+++ packages/py-moose-lib/setup.py
@@ -1,6 +1,11 @@
 import sys
 from setuptools import setup, find_packages
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 version = "0.0.0"
 
 if '--version' in sys.argv:
@@ -12,6 +17,11 @@ setup(
     name='moose_lib',
     version=version,
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Fiveonefour Labs Inc.',
+    author_email='support@fiveonefour.com',
+    url='https://www.fiveonefour.com/moose',
     install_requires=[
         'pyjwt[crypto]==2.9.0',
         'asyncio==3.4.3',

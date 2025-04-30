90902c1c66a2a9a48b5740cbc2f044e6d12332fd
Olivia Kane <67128565+okane16@users.noreply.github.com>
2025-04-30 14:13:13 -0400

    remove stuff breaking (#2287)
    * remove stuff breaking
    
    * tailwind config

--- templates/github-dev-trends/dashboard/lib/moose-client.ts
+++ templates/github-dev-trends/dashboard/lib/moose-client.ts
@@ -1,13 +1,4 @@
-import {
-  DefaultApi,
-  Configuration,
-  ConsumptionTopicTimeseriesGetRequest,
-  ConsumptionTopicTimeseriesGet200ResponseInner,
-} from "api-client";
-
-export type TopicTimeseriesRequest = ConsumptionTopicTimeseriesGetRequest;
-export type TopicTimeseriesResponse =
-  ConsumptionTopicTimeseriesGet200ResponseInner;
+import { DefaultApi, Configuration } from "api-client";
 
 const mooseUrl = process.env.MOOSE_URL || "http://localhost:4000";
 

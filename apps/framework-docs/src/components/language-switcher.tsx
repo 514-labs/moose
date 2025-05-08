d39aff4df351972916ee9c2de990d914581923e9
Alex <alex.lakas@gmail.com>
2025-05-08 08:14:06 -0700

    Replace text language switcher with Shadcn dropdown component

--- apps/framework-docs/src/components/language-switcher.tsx
+++ apps/framework-docs/src/components/language-switcher.tsx
@@ -15,31 +15,36 @@ export function LanguageSwitcher() {
   const otherLanguage = language === "python" ? "typescript" : "python";
 
   return (
-    <div className="my-5 flex flex-row w-full border-t border-b justify-between bg-black sticky top-20 z-99999">
-      <Heading level={HeadingLevel.l5} className="text-captialize">
-        Viewing {language}
+    // <div className="my-5 flex flex-row w-full border-t border-b justify-between bg-black sticky top-20 z-99999999">
+    //   <Heading level={HeadingLevel.l5} className="text-captialize">
+    //     Viewing {language}
+    //   </Heading>
+    //   <Heading level={HeadingLevel.l5}>
+    //     switch to{" "}
+    //     <span
+    //       className="text-moose-purple hover:cursor-pointer"
+    //       onClick={() => setLanguage(otherLanguage)}
+    //     >
+    //       {otherLanguage}
+    //     </span>
+    //   </Heading>
+    // </div>
+    <div className="sticky top-[64px] z-50 py-2 border-t-0 bg-background border-y flex items-center justify-between">
+      <Heading level={HeadingLevel.l5} className="text-capitalize">
+        Viewing
       </Heading>
-      <Heading level={HeadingLevel.l5}>
-        switch to{" "}
-        <span
-          className="text-moose-purple hover:cursor-pointer"
-          onClick={() => setLanguage(otherLanguage)}
-        >
-          {otherLanguage}
-        </span>
-      </Heading>
+      <Select
+        value={language}
+        onValueChange={(value: "typescript" | "python") => setLanguage(value)}
+      >
+        <SelectTrigger className="w-[auto]">
+          <SelectValue placeholder="Select Language" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="typescript">TypeScript</SelectItem>
+          <SelectItem value="python">Python</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
-    // <Select
-    //   value={language}
-    //   onValueChange={(value: "typescript" | "python") => setLanguage(value)}
-    // >
-    //   <SelectTrigger className="w-[180px]">
-    //     <SelectValue placeholder="Select Language" />
-    //   </SelectTrigger>
-    //   <SelectContent>
-    //     <SelectItem value="typescript">TypeScript</SelectItem>
-    //     <SelectItem value="python">Python</SelectItem>
-    //   </SelectContent>
-    // </Select>
   );
 }

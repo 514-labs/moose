1421d6f7d43e1584f2421edb6163a36fee12058b
Olivia Kane <67128565+okane16@users.noreply.github.com>
2025-05-28 08:33:58 -0700

    From clickhouse docs (#2378)
    * from ch
    
    * limitations
    
    * simplify ch playground
    
    * disclaimer
    
    * add context
    
    * formatting
    
    * final formatting tweaks
    
    * title
    
    * cursed knowledge
    
    * add ctas higher up to get started from clickhouse

--- apps/framework-docs/src/pages/moose/getting-started/_meta.tsx
+++ apps/framework-docs/src/pages/moose/getting-started/_meta.tsx
@@ -2,6 +2,7 @@ import { render } from "@/components";
 
 const meta = {
   quickstart: "Quickstart",
+  "from-clickhouse": "Start from Existing ClickHouse",
   "project-structure": "Project Structure",
   architecture: "Architecture",
 };

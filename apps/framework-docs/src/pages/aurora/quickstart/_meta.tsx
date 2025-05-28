6c43512e692f91dbf9c8f78fb6887e8eff4a57a1
Johanan Ottensooser <j@ottensooser.com>
2025-05-28 10:03:45 -1000

    docs: restructure docs (#2386)
    This pull request restructures the Aurora documentation by introducing reorganizing the Quickstart guides into individual pages, and updating navigation and content to improve clarity and usability.

---
+++ apps/framework-docs/src/pages/aurora/quickstart/_meta.tsx
@@ -0,0 +1,8 @@
+import { render } from "@/components";
+const meta = {
+  index: { title: "Quickstart Guides", display: "hidden" },
+  "clickhouse-chat": "Chat with Clickhouse",
+  "clickhouse-proj": "Build analytics project from Clickhouse",
+  "from-template": "Full stack OLAP project from Template",
+};
+export default render(meta);

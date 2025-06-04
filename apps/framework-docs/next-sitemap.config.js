f166d3e02fecbae9af6333fd345623eb072a3a31
Nicolas Joseph <nicolas@fiveonefour.com>
2025-06-04 13:14:50 -0700

    chore: remove auto generation of robot.txt (#2415)

--- apps/framework-docs/next-sitemap.config.js
+++ apps/framework-docs/next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://docs.fiveonefour.com/",
-  generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: false, // (optional)
   exclude: ["*/_meta"],
   // ...other options
 };

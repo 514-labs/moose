b9c441fcdfd6d507657c1dccc42705c46a92a36a
Nicolas Joseph <nicolas@fiveonefour.com>
2025-06-09 08:39:10 -0700

    chore: move workflows and Tasks to their own file

--- packages/ts-moose-lib/src/dmv2/index.ts
+++ packages/ts-moose-lib/src/dmv2/index.ts
@@ -475,52 +475,6 @@ export class MaterializedView<TargetTable> extends SqlResource {
   }
 }
 
-type TaskHandler<T, R> = (input: T) => Promise<R | void>;
-
-export interface TaskConfig<T, R> {
-  run: TaskHandler<T, R>;
-  onComplete?: Task<R, any>[];
-  timeout?: string;
-  retries?: number;
-}
-
-export class Task<T, R = any> extends TypedBase<T, TaskConfig<T, R>> {
-  constructor(name: string, config: TaskConfig<T, R>);
-
-  /** @internal **/
-  constructor(
-    name: string,
-    config: TaskConfig<T, R>,
-    schema: IJsonSchemaCollection.IV3_1,
-    columns: Column[],
-  );
-
-  constructor(
-    name: string,
-    config: TaskConfig<T, R>,
-    schema?: IJsonSchemaCollection.IV3_1,
-    columns?: Column[],
-  ) {
-    super(name, config, schema, columns);
-  }
-}
-
-export interface WorkflowConfig {
-  startingTask: Task<any, any>;
-  retries?: number;
-  timeout?: string;
-  schedule?: string;
-}
-
-export class Workflow {
-  constructor(
-    readonly name: string,
-    readonly config: WorkflowConfig,
-  ) {
-    getMooseInternal().workflows.set(name, this);
-  }
-}
-
 export { OlapTable, OlapConfig } from "./sdk/olapTable";
 export {
   Stream,
@@ -529,3 +483,5 @@ export {
   DeadLetter,
   DeadLetterQueue,
 } from "./sdk/stream";
+
+export { Workflow, Task } from "./sdk/workflow";

c3cdaec03e43750a6d6cc6d3ccb59939e71e1b4a
Nicolas Joseph <nicolas@fiveonefour.com>
2025-07-09 14:03:52 -0700

    chore: Remove old cron functionality (#2531)

--- apps/framework-cli/src/framework/core/infra_reality_checker.rs
+++ apps/framework-cli/src/framework/core/infra_reality_checker.rs
@@ -283,7 +283,7 @@ mod tests {
             supported_old_versions: std::collections::HashMap::new(),
             jwt: None,
             authentication: crate::project::AuthenticationConfig::default(),
-            cron_jobs: Vec::new(),
+
             features: crate::project::ProjectFeatures::default(),
             load_infra: None,
         }

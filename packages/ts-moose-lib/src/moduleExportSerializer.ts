81d9162e1715e96d5b27c6e7602df2ccb743db45
Nicolas Joseph <nicolas@fiveonefour.com>
2025-08-01 10:49:21 -0400

    fix broke some xport

--- packages/ts-moose-lib/src/moduleExportSerializer.ts
+++ packages/ts-moose-lib/src/moduleExportSerializer.ts
@@ -1,6 +1,4 @@
-import { compilerLog } from "./commons";
-
 export async function runExportSerializer(targetModel: string) {
   const exports_list = require(targetModel);
-  compilerLog(JSON.stringify(exports_list));
+  console.log(JSON.stringify(exports_list));
 }

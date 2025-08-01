cda2da18ee971e3715b223a6c746a56763cfd4ef
Olivia Kane <olivia@fiveonefour.com>
2025-07-31 20:15:00 -0400

    stream, build migration

--- apps/framework-docs/src/pages/stack/apis/_meta.tsx
+++ apps/framework-docs/src/pages/stack/apis/_meta.tsx
@@ -1,11 +1,14 @@
 import { render } from "@/components";
 
 const rawMeta = {
-  "ingest-data": {
+  "ingest-api": {
     title: "Configure Ingest Endpoints",
   },
-  "egress-data": {
+  "query-api": {
     title: "Expose Queries as APIs",
+  },
+  "trigger-api": {
+    title: "Trigger Workflows from APIs",
   },
   auth: {
     title: "Securing API Endpoints",
---
+++ apps/framework-docs/src/pages/stack/streaming/_meta.tsx
@@ -0,0 +1,36 @@
+import { render } from "@/components";
+
+const rawMeta = {
+  "---Managing Streams---": {
+    title: "Managing Streams",
+    type: "separator",
+  },
+  "create-stream": {
+    title: "Creating Streams",
+  },
+  "sync-to-table": {
+    title: "Syncing Streams to Tables",
+  },
+  "---Functions---": {
+    title: "Functions",
+    type: "separator",
+  },
+  "consumer-functions": {
+    title: "Consumer Functions",
+  },
+  "transform-functions": {
+    title: "Transformation Functions",
+  },
+  "---Producing---": {
+    title: "Writing to Streams",
+    type: "separator",
+  },
+  producers: {
+    title: "From Your Code",
+  },
+  "connect-cdc": {
+    title: "From CDC Services",
+  },
+};
+
+export default render(rawMeta);

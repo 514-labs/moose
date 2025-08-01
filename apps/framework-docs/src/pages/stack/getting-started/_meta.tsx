6353514dd123a4ebe0901e4a390fb61448ee16ad
Olivia Kane <olivia@fiveonefour.com>
2025-07-31 20:23:49 -0400

    fix stream publish

--- apps/framework-docs/src/pages/stack/getting-started/_meta.tsx
+++ apps/framework-docs/src/pages/stack/getting-started/_meta.tsx
@@ -3,8 +3,6 @@ import { render } from "@/components";
 const meta = {
   quickstart: "5-Minute Quickstart",
   "from-clickhouse": "Use with Existing ClickHouse",
-  "core-concepts": "Core Concepts",
-  architecture: "Reference Architectures",
   "project-structure": "Project Organization",
 };
 

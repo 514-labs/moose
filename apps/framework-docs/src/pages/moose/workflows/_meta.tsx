04ca75b3b5f96fda7ee3d5c1c058a15563f74b88
Olivia Kane <67128565+okane16@users.noreply.github.com>
2025-08-05 21:57:45 -0400

    Olivia/moosestack workflows (#2615)
    workflows new content and structure. includes all comments from original
    PR from @DatGuyJonathan
    
    keeping hidden until all other merges are in.
    
    2 broken links for now because they are dependent on new content from
    other module sections

---
+++ apps/framework-docs/src/pages/moose/workflows/_meta.tsx
@@ -0,0 +1,21 @@
+import { render } from "@/components";
+
+export const meta = {
+  "define-workflow": {
+    title: "Define Workflows",
+  },
+  "schedule-workflow": {
+    title: "Scheduling",
+  },
+  "trigger-workflow": {
+    title: "Triggers",
+  },
+  "retries-and-timeouts": {
+    title: "Retries and Timeouts",
+  },
+  "cancel-workflow": {
+    title: "Cancelling Running Workflows",
+  },
+};
+
+export default render(meta);

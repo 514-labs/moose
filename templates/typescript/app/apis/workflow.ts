a90fcdb9bfd623990086948a6589dccfa46f7381
George Anderson <georgevanderson@gmail.com>
2025-08-15 12:17:31 -0600

    Add terminate to workflow client (#2648)

---
+++ templates/typescript/app/apis/workflow.ts
@@ -0,0 +1,34 @@
+import { ConsumptionApi } from "@514labs/moose-lib";
+
+interface WorkflowResponse {
+  workflowId: string;
+  status: string;
+}
+
+const triggerApi = new ConsumptionApi<{}, WorkflowResponse>(
+  "start-workflow",
+  async (_, { client }) => {
+    // Trigger the workflow with input parameters
+    const workflowExecution = await client.workflow.execute("workflow", {});
+
+    return {
+      workflowId: workflowExecution.body,
+      status: "started",
+    };
+  },
+);
+
+const terminateAPI = new ConsumptionApi<{}, WorkflowResponse>(
+  "terminate-workflow",
+  async (_, { client }) => {
+    // Terminate the workflow by name
+    const workflowExecution = await client.workflow.terminate("workflow");
+
+    return {
+      workflowId: workflowExecution.body,
+      status: "terminated",
+    };
+  },
+);
+
+export { triggerApi, terminateAPI };

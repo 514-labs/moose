3114fd6566f10134b99847585c82bbdbab39eb60
Olivia Kane <olivia@fiveonefour.com>
2025-08-22 16:05:08 -0600

    change workflow name

--- templates/typescript/app/apis/workflow.ts
+++ templates/typescript/app/apis/workflow.ts
@@ -6,11 +6,7 @@ interface WorkflowResponse {
 }
 
 const triggerApi = new Api<{}, WorkflowResponse>(
-<<<<<<< HEAD
-  "start-workflow",
-=======
   "start-generator-workflow",
->>>>>>> 7938e271 (APis)
   async (_, { client }) => {
     // Trigger the workflow with input parameters
     const workflowExecution = await client.workflow.execute("generator", {});
@@ -23,11 +19,7 @@ const triggerApi = new Api<{}, WorkflowResponse>(
 );
 
 const terminateAPI = new Api<{}, WorkflowResponse>(
-<<<<<<< HEAD
-  "terminate-workflow",
-=======
   "terminate-generator-workflow",
->>>>>>> 7938e271 (APis)
   async (_, { client }) => {
     // Terminate the workflow by name
     const workflowExecution = await client.workflow.terminate("generator");
